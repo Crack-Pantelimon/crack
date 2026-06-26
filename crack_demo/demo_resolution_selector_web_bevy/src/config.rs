eafa95be7d3c8fc4c3256ae60954dbf50558ff00
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2026-06-26 19:51:55 +0300

    deploy

--- crack_demo/demo_resolution_selector_web_bevy/src/config.rs
+++ crack_demo/demo_resolution_selector_web_bevy/src/config.rs
@@ -1,1 +1,6 @@
-pub const DATA_BASE_URL: &str = "http://localhost:1973";
+
+
+    #[cfg(feature = "web")]
+    pub const DATA_BASE_URL: &str = "https://pantelimon.alt-f4.ro/";
+    #[cfg(not(feature = "web"))]
+    pub const DATA_BASE_URL: &str = "http://127.0.0.1:1973/";
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+(
+    cd crack_demo/demo_resolution_selector_web_bevy
+    trunk build --release true
+)
+
 rsync -av --exclude 'target' --exclude '.git' ./ dj-vaslui:crack/
 
 ssh dj-vaslui "cd crack && cd _data && docker compose up -d"
